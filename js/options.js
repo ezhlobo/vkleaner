8f0e00cd3d277615e11fb5fa9fd524907d7abbce
Evgeny Zhlobo <evgen.zhlobo@gmail.com>
2012-03-09 19:58:23 +0300

    Replace tabs with spaces

--- js/options.js
+++ js/options.js
@@ -1,101 +1,101 @@
 $(function(){
-	localStorageManager.get(idsBlocksWithOptions + ", clearvk.sites");
-	setTimeout(setDefaultOptions, 300);
+  localStorageManager.get(idsBlocksWithOptions + ", clearvk.sites");
+  setTimeout(setDefaultOptions, 300);
 
-	$("#wrap")
-		.load("optionsItems.html")
-		.on("click", "#clearvk_linksToAsks a", function () {
-			notification.show("sites");
-			return false;
-		})
-		.on("click", ".savebutton", saveParams);
+  $("#wrap")
+    .load("optionsItems.html")
+    .on("click", "#clearvk_linksToAsks a", function () {
+      notification.show("sites");
+      return false;
+    })
+    .on("click", ".savebutton", saveParams);
 });
 
 var defaultSites = ["ask.fm", "sprashivai.ru", "formspring.me", "my-truth.ru", "askbook.me", "askme.by", "qroom.ru", "nekto.me"]
 var idsBlocksWithOptions = "clearvk_class, clearvk_repostFromGroups, clearvk_linksToAsks, clearvk_video, clearvk_audio";
 var localStorageManager = {
-	set: function (name, value) {
-		chrome.extension.sendRequest({type:"set", name:name, value:value});
-	},
-	fnGET: function (name) {
-		chrome.extension.sendRequest({type:"get", name:name}, function (response) {
-			ownLocalStorage[name] = response || 1;
-		});
-	},
-	get: function (ids) {
-		collectionIds = ids.split(", ");
-		for (var k in collectionIds) localStorageManager.fnGET(collectionIds[k]);
-	}
+  set: function (name, value) {
+    chrome.extension.sendRequest({type:"set", name:name, value:value});
+  },
+  fnGET: function (name) {
+    chrome.extension.sendRequest({type:"get", name:name}, function (response) {
+      ownLocalStorage[name] = response || 1;
+    });
+  },
+  get: function (ids) {
+    collectionIds = ids.split(", ");
+    for (var k in collectionIds) localStorageManager.fnGET(collectionIds[k]);
+  }
 }, ownLocalStorage = {};
 
 var getBadUrls = function () {
-	var urls = ownLocalStorage["clearvk.sites"];
-	return (urls == 1) ? defaultSites : urls.split(",");
+  var urls = ownLocalStorage["clearvk.sites"];
+  return (urls == 1) ? defaultSites : urls.split(",");
 }
 
 var setDefaultOptions = function () {
-	for (var id in ownLocalStorage) $("#"+id).find("input").removeAttr("checked").filter("[value="+ownLocalStorage[id]+"]").attr("checked", "checked");
+  for (var id in ownLocalStorage) $("#"+id).find("input").removeAttr("checked").filter("[value="+ownLocalStorage[id]+"]").attr("checked", "checked");
 }
 var saveParams = function () {
-	var self = $(this);
-	$(".option").each(function(){
-		localStorageManager.set($(this).attr("id"), $(this).find("input:checked").val());
-	});
-	showStatusMessage();
+  var self = $(this);
+  $(".option").each(function(){
+    localStorageManager.set($(this).attr("id"), $(this).find("input:checked").val());
+  });
+  showStatusMessage();
 }
 
 var showStatusMessage = function () {
-	notification.hide();
-	$("#status").show();
-	setTimeout(function(){ $("#status").hide(); }, 600);
+  notification.hide();
+  $("#status").show();
+  setTimeout(function(){ $("#status").hide(); }, 600);
 }
 var notification = {
-	show: function (id) {
-		notification
-			.background.show()
-			.animation("show", "+", notification.content(id))
-			.addTriggerSave(id);
-	},
-	hide: function () {
-		notification
-			.background.hide()
-			.animation("hide", "-", "")
-			.removeTriggerSave();
-	},
-	addTriggerSave: function (id) {
-		$("#notifier button").on("click", function () {
-			localStorageManager.set("clearvk.sites", notification.content(id, "save"));
-			setTimeout(showStatusMessage, 300);
-		});
-	},
-	removeTriggerSave: function () { $("#notifier button").off("click"); },
-	animation: function (valueOpacity, symbolTop, html) {
-		$("#notifier").animate({opacity: valueOpacity, marginTop: symbolTop+"=20px"}, 100).children(".notifier").html(html);
-		return this;
-	},
-	background: {
-		show: function () {
-			$("body").append("<div class=\"background\"></div>");
-			$(".background").width($(document).width()).height($(document).height()).on("click", function(){
-				$(this).off("click");
-				notification.hide();
-			});
-			return notification;
-		},
-		hide: function () {
-			$(".background").off("click").remove();
-			return notification;
-		}
-	},
-	content: function (id, way) {
-		switch (id) {
-			case "sites":
-				if (way == "save") {
-					return $("#notifier textarea").val().trim().split("\n");
-				} else {
-					return "<p class=\"title\">Для восстановления введите 1 и сохраните</p><textarea class=\"clearvk-id2\">"+getBadUrls().join("\n")+"</textarea>";
-				}
-				break;
-		}
-	}
+  show: function (id) {
+    notification
+      .background.show()
+      .animation("show", "+", notification.content(id))
+      .addTriggerSave(id);
+  },
+  hide: function () {
+    notification
+      .background.hide()
+      .animation("hide", "-", "")
+      .removeTriggerSave();
+  },
+  addTriggerSave: function (id) {
+    $("#notifier button").on("click", function () {
+      localStorageManager.set("clearvk.sites", notification.content(id, "save"));
+      setTimeout(showStatusMessage, 300);
+    });
+  },
+  removeTriggerSave: function () { $("#notifier button").off("click"); },
+  animation: function (valueOpacity, symbolTop, html) {
+    $("#notifier").animate({opacity: valueOpacity, marginTop: symbolTop+"=20px"}, 100).children(".notifier").html(html);
+    return this;
+  },
+  background: {
+    show: function () {
+      $("body").append("<div class=\"background\"></div>");
+      $(".background").width($(document).width()).height($(document).height()).on("click", function(){
+        $(this).off("click");
+        notification.hide();
+      });
+      return notification;
+    },
+    hide: function () {
+      $(".background").off("click").remove();
+      return notification;
+    }
+  },
+  content: function (id, way) {
+    switch (id) {
+      case "sites":
+        if (way == "save") {
+          return $("#notifier textarea").val().trim().split("\n");
+        } else {
+          return "<p class=\"title\">Для восстановления введите 1 и сохраните</p><textarea class=\"clearvk-id2\">"+getBadUrls().join("\n")+"</textarea>";
+        }
+        break;
+    }
+  }
 }
--- js/vk.feed.js
+++ js/vk.feed.js
@@ -1,85 +1,85 @@
 var defaultSites = ["ask.fm", "sprashivai.ru", "formspring.me", "my-truth.ru", "askbook.me", "askme.by", "qroom.ru", "nekto.me", "trislovamne.ru"];
 var idsBlocksWithOptions = "clearvk_class, clearvk_repostFromGroups, clearvk_linksToAsks, clearvk_video, clearvk_audio";
 var localStorageManager = {
-	fnGET: function (name) {
-		chrome.extension.sendRequest({type:"get", name:name}, function (response) {
-			ownLocalStorage[name] = response || 1;
-		});
-	},
-	get: function (ids) {
-		collectionIds = ids.split(", ");
-		for (var k in collectionIds) localStorageManager.fnGET(collectionIds[k]);
-	}
+  fnGET: function (name) {
+    chrome.extension.sendRequest({type:"get", name:name}, function (response) {
+      ownLocalStorage[name] = response || 1;
+    });
+  },
+  get: function (ids) {
+    collectionIds = ids.split(", ");
+    for (var k in collectionIds) localStorageManager.fnGET(collectionIds[k]);
+  }
 }, ownLocalStorage = {};
 localStorageManager.get(idsBlocksWithOptions + ", clearvk.sites");
 
 var getBadUrls = function () {
-	var urls = ownLocalStorage["clearvk.sites"];
-	return (urls == 1) ? defaultSites : urls.split(",");
+  var urls = ownLocalStorage["clearvk.sites"];
+  return (urls == 1) ? defaultSites : urls.split(",");
 }
 
 var cssClassForHiddenPosts;
 var hidePosts = {
-	fromGroups: function (post) {
-		var repost = post.find(".published_by");
-		if (repost.length > 0) {
-			var currentRow = post.closest(".feed_row");
-			var cssClass = currentRow.children().attr("class");
-			var isRepostGroup = cssClass.substr(17, 1) == "-";
-			var isRepostSingle = cssClass.substr(11, 1) == "-";
-			var isRepostPhotos = repost.siblings(".published_by_date").children("a").attr("href").substr(6, 1) == "-";
-			
-			if (isRepostGroup || isRepostSingle || isRepostPhotos)
-				this.addCssClass(post);
-			if (isRepostGroup)
-				currentRow.find(".feed_reposts_more").addClass(cssClassForHiddenPosts+"-group");
-		}
-	},
-	withLinks: function (post) {
-		var hrefMediaLink = unescape(post.find(".media_desc").find(".lnk").attr("href"));
-		var hrefLinkInText = unescape(post.find(".wall_post_text").find("a").attr("href"));
-		var postText = post.find(".wall_post_text").text();
-		
-		var templateHref = new RegExp("(http:\\/\\/|w{3}\\.|\s)("+ getBadUrls().join("|").replace(/\./gi, "\\.") +")", "gi");
-		if (templateHref.test(hrefMediaLink) || templateHref.test(hrefLinkInText) || templateHref.test(postText))
-			this.addCssClass(post);
-	},
-	withVideo: function (post) {
-		var video = post.find(".page_media_video");
-		if (video.length > 0)
-			this.addCssClass(post);
-	},
-	withAudio: function (post) {
-		var audio = post.find(".audio");
-		if (audio.length > 0)
-			this.addCssClass(post);
-	},
-	addCssClass: function (post) { post.addClass(cssClassForHiddenPosts) }
+  fromGroups: function (post) {
+    var repost = post.find(".published_by");
+    if (repost.length > 0) {
+      var currentRow = post.closest(".feed_row");
+      var cssClass = currentRow.children().attr("class");
+      var isRepostGroup = cssClass.substr(17, 1) == "-";
+      var isRepostSingle = cssClass.substr(11, 1) == "-";
+      var isRepostPhotos = repost.siblings(".published_by_date").children("a").attr("href").substr(6, 1) == "-";
+
+      if (isRepostGroup || isRepostSingle || isRepostPhotos)
+        this.addCssClass(post);
+      if (isRepostGroup)
+        currentRow.find(".feed_reposts_more").addClass(cssClassForHiddenPosts+"-group");
+    }
+  },
+  withLinks: function (post) {
+    var hrefMediaLink = unescape(post.find(".media_desc").find(".lnk").attr("href"));
+    var hrefLinkInText = unescape(post.find(".wall_post_text").find("a").attr("href"));
+    var postText = post.find(".wall_post_text").text();
+
+    var templateHref = new RegExp("(http:\\/\\/|w{3}\\.|\s)("+ getBadUrls().join("|").replace(/\./gi, "\\.") +")", "gi");
+    if (templateHref.test(hrefMediaLink) || templateHref.test(hrefLinkInText) || templateHref.test(postText))
+      this.addCssClass(post);
+  },
+  withVideo: function (post) {
+    var video = post.find(".page_media_video");
+    if (video.length > 0)
+      this.addCssClass(post);
+  },
+  withAudio: function (post) {
+    var audio = post.find(".audio");
+    if (audio.length > 0)
+      this.addCssClass(post);
+  },
+  addCssClass: function (post) { post.addClass(cssClassForHiddenPosts) }
 }
 var hideSomePosts = function () {
-	$("#feed_rows").find(".post").each(function(){
-		$(this).removeClass(cssClassForHiddenPosts);
-		for (var name in whatNeedHide)
-			hidePosts[ whatNeedHide[name] ] ( $(this) );
-	});
+  $("#feed_rows").find(".post").each(function(){
+    $(this).removeClass(cssClassForHiddenPosts);
+    for (var name in whatNeedHide)
+      hidePosts[ whatNeedHide[name] ] ( $(this) );
+  });
 }
 
 var checkLocation = function () {
-	if (window.location.pathname == "/feed")
-		hideSomePosts();
+  if (window.location.pathname == "/feed")
+    hideSomePosts();
 }
 setInterval(checkLocation, 500);
 setTimeout(function () {
-	whatNeedHide = [];
-	if (ownLocalStorage["clearvk_repostFromGroups"] == 1)
-		whatNeedHide.push("fromGroups");
-	if (ownLocalStorage["clearvk_linksToAsks"] == 1)
-		whatNeedHide.push("withLinks");
-	if (ownLocalStorage["clearvk_video"] == 1)
-		whatNeedHide.push("withVideo");
-	if (ownLocalStorage["clearvk_audio"] == 1)
-		whatNeedHide.push("withAudio");
-	
-	cssClassForHiddenPosts = (ownLocalStorage["clearvk_class"] == 1) ? "clearvk-showTop" : "clearvk-hideAll";
-	hideSomePosts();
+  whatNeedHide = [];
+  if (ownLocalStorage["clearvk_repostFromGroups"] == 1)
+    whatNeedHide.push("fromGroups");
+  if (ownLocalStorage["clearvk_linksToAsks"] == 1)
+    whatNeedHide.push("withLinks");
+  if (ownLocalStorage["clearvk_video"] == 1)
+    whatNeedHide.push("withVideo");
+  if (ownLocalStorage["clearvk_audio"] == 1)
+    whatNeedHide.push("withAudio");
+
+  cssClassForHiddenPosts = (ownLocalStorage["clearvk_class"] == 1) ? "clearvk-showTop" : "clearvk-hideAll";
+  hideSomePosts();
 }, 100);
